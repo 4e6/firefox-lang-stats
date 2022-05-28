38236f8d87f322f51676f897c6c314fbe091e7e7
Anatoli Babenia <anatoli@rainforce.org>
2022-05-28 08:04:04 +0300

    Use absolute path for a build dir

--- 01stats.sh
+++ 01stats.sh
@@ -2,16 +2,16 @@
 
 REPODIR=${1:-.}   # default to . if first argument is not set
 SCRIPTDIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
-BUILDDIR=${2:-$SCRIPTDIR}
+BUILDDIR=$(readlink -f "${2:-$SCRIPTDIR}")
 
 [[ -d $BUILDDIR ]] || { echo "$BUILDDIR dir does not exist"; exit 1; }
 
 (cd "$REPODIR" || exit
 
-  echo "commits per day.."
+  echo "writing $BUILDDIR/commits-per-day.txt"
   git log --date=short --format="%cd" --reverse --topo-order |
     uniq -c > "$BUILDDIR"/commits-per-day.txt
 
-  echo "git exension stats.."
+  echo "writing $BUILDDIR/extensions.txt"
   "$SCRIPTDIR"/dev/git-file-stats > "$BUILDDIR/extensions.txt"
 )
